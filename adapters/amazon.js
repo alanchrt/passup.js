202e2cf9937953a9e26e9c9298d763c0f8f78ad9
Alan Christopher Thomas <alan@alanct.com>
2013-06-23 23:09:51 -0500

    added amazon regex

--- adapters/amazon.js
+++ adapters/amazon.js
@@ -1,6 +1,6 @@
 exports.adapter = {
     "name": "Amazon",
-    "passwordRegExp": /\w/,
+    "passwordRegExp": /^.{6,128}$/, // 6 to 128 characters
     "update": function(data) {
         casper.start('https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fcss%2Fhomepage.html%3Fie%3DUTF8%26ref_%3Dgno_yam_ya');
 
