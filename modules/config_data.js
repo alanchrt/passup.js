a419dc45ae58173abe80969a66dfe5d31106f8aa
Coalman <tehcoalman@gmail.com>
2013-07-09 16:03:03 -0400

    Created ConfigData module for loading json config.

---
+++ modules/config_data.js
@@ -0,0 +1,35 @@
+fs = require('fs');
+
+// ConfigData
+//-----------------------------------------------
+
+function ConfigData(rawData, cleanData) {
+	this.rawData = rawData;
+	this.cleanData = cleanData;
+}
+
+ConfigData.load = function(filePath) {
+	configJson = fs.read(filePath);
+	rawData = JSON.parse(configJson);
+	cleanData = JSON.parse(configJson);
+
+	return new ConfigData(rawData, cleanData);
+};
+
+ConfigData.prototype.filterParams = function(params) {
+	if (params.passwords) {
+		this.cleanData.passwords = this.cleanData.passwords.filter(
+			function(password, index, passwords) {
+				return params.isSpecifiedPassword(password.name);
+			}, params);
+	}
+	if (params.adapters) {
+		this.cleanData.passwords.forEach(function(password, index, passwords) {
+			password.sites = password.sites.filter(function(site, index, sites) {
+				return params.isSpecifiedAdapter(site.adapter);
+			});
+		});
+	}
+};
+
+module.exports = ConfigData;
