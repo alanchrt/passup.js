ceb74b4cde152821bc15cc63c66b858bbed1b278
Alan Christopher Thomas <alan@alanct.com>
2013-07-12 11:31:38 -0500

    retabbing

--- modules/configData.js
+++ modules/configData.js
@@ -4,43 +4,46 @@ fs = require('fs');
 //-----------------------------------------------
 
 function ConfigData(rawData, cleanData) {
-	this.rawData = rawData;
-	this.cleanData = cleanData;
+    this.rawData = rawData;
+    this.cleanData = cleanData;
 }
 
 ConfigData.load = function(filePath) {
-	configJson = fs.read(filePath);
+    configJson = fs.read(filePath);
 
-	// load the json twice to make two deep copies.
-	rawData = JSON.parse(configJson);
-	cleanData = JSON.parse(configJson);
+    // load the json twice to make two deep copies.
+    rawData = JSON.parse(configJson);
+    cleanData = JSON.parse(configJson);
 
-	return new ConfigData(rawData, cleanData);
+    return new ConfigData(rawData, cleanData);
 };
 
 ConfigData.prototype.filterParams = function(params) {
-	// filter passwords based on command line arguments
-	if (params.passwords) {
-		this.cleanData.passwords = this.cleanData.passwords.filter(
-			function(password, index, passwords) {
-				return params.isSpecifiedPassword(password.name);
-			}, params);
-	}
+    // filter passwords based on command line arguments
+    if (params.passwords) {
+        this.cleanData.passwords = this.cleanData.passwords.filter(
+            function(password, index, passwords) {
+                return params.isSpecifiedPassword(password.name);
+            },
+            params
+        );
+    }
 
-	// filter sites based on command line arguments
-	if (params.adapters) {
-		this.cleanData.passwords.forEach(function(password, index, passwords) {
-			password.sites = password.sites.filter(function(site, index, sites) {
-				return params.isSpecifiedAdapter(site.adapter);
-			});
-		});
-	}
+    // filter sites based on command line arguments
+    if (params.adapters) {
+        this.cleanData.passwords.forEach(function(password, index, passwords) {
+            password.sites = password.sites.filter(function(site, index, sites) {
+                return params.isSpecifiedAdapter(site.adapter);
+            });
+        });
+    }
 
-	// remove passwords that have no sites to update
-	this.cleanData.passwords = this.cleanData.passwords.filter(
-		function(password, index, passwords) {
-			return (password.sites.length > 0);
-		});
+    // remove passwords that have no sites to update
+    this.cleanData.passwords = this.cleanData.passwords.filter(
+        function(password, index, passwords) {
+            return (password.sites.length > 0);
+        }
+    );
 
     return this;
 };
